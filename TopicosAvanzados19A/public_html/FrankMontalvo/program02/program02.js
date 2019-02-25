871cd476be5ede6b7e390cfc1bf895685a8800f2
Frank Montalvo <framohi@hotmail.com>
2019-02-24 19:59:59 -0600

    program02

---
+++ TopicosAvanzados19A/public_html/FrankMontalvo/program02/program02.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* global NaN */
+
+function leerCalificaciones() {
+    var grupo = document.getElementById("inputGrupo").value;
+    var cal = [];
+    var suma = 0;
+    var i;
+    for (i = 0; i < grupo; i++) {
+        while (true) {
+            var calificaciones = prompt("escribe la calificación del alumno");
+            cal[i] = Number(calificaciones);
+            if (cal[i] % 1 === 0 && cal[i] >= 0 && cal[i] <= 100) {
+                break;
+            } else {
+                alert("No es una calificacion valida");
+            }
+        }
+        suma = suma + cal[i];
+    }
+    document.write(suma / grupo);
+}
