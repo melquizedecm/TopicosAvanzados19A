cb0a9d1d9787720f5241200264f7cbfcc94529e1
unknown <juanmezquitauc@hotmail.com>
2019-02-24 22:02:51 -0600

    modificaciones

--- TopicosAvanzados19A/public_html/JuanMezquita/Programa02.js
+++ TopicosAvanzados19A/public_html/JuanMezquita/Programa02.js
@@ -1,21 +1,20 @@
-var n;
-var cal=[];
-var suma=0;
-var promedio;
-var i;
-
-
-leerCalificaciones();
-
-function leerCalificaciones(){
-
-alert("ia iegue");
-
-/*alert(n);
-
-/*do{
-  n=prompt("escribe un numero");
-
-}while(n>0);
-*/
+function leerCalificaciones() {
+    var grupo = document.getElementById("inputGrupo").value;
+    var cal = [];
+    var suma = 0;
+    var i;
+    for (i = 0; i < grupo; i++) {
+        while (true) {
+            var calificaciones = prompt("escribe la calificación del alumno");
+            cal[i] = Number(calificaciones);
+            if (cal[i] % 1 === 0 && cal[i] >= 0 && cal[i] <= 100) {
+                break;
+            } else {
+                alert("No es una calificacion valida");
+            }
+        }
+        suma = suma + cal[i];
+    }
+    document.write(suma / grupo);
 }
+leerCalificaciones();
