6ebd8444fc63d567e5a40abbd66ce96fc8bfcaa7
KennAyala <kennayala@outlook.com>
2019-02-24 20:58:54 -0600

    Actualizado programas

---
+++ TopicosAvanzados19A/public_html/KennAyala/Programa02.js
@@ -0,0 +1,16 @@
+var n;
+var cal = [];
+var suma = 0;
+var promedio = 0;
+var i;
+var t = prompt("Ingrese el tamaño del grupo");
+leerCalificaciones();
+
+function leerCalificaciones() {
+	for (i; i < t; i++) {
+		do {
+			n = prompt("Escribe la cantidad de calificaciones");
+		} while ((n < 0) || n % 1 !== 0);
+	}
+	alert(n);
+}
