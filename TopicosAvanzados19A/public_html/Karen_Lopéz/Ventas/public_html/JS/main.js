a949c0a3788cb39633ab644f1f1d628f62a0094f
KarenJanett <pe.ke2010@hotmail.com>
2019-02-25 02:16:34 -0600

    se agrego ventas al proyecto integrador

---
+++ TopicosAvanzados19A/public_html/Karen_Lopéz/Ventas/public_html/JS/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+        $('#example').DataTable({
+            scrollY:        '20vh',
+            scrollCollapse: true,
+            paging:         false
+        } );
+    } );
