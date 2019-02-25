70d653b1d7e5bb2d0f22d8f499e15525d323eadc
RonalGarcia <ronincubus360@hotmail.com>
2019-02-24 19:43:28 -0600

    update

---
+++ TopicosAvanzados19A/public_html/Oscar_Uh/miCodigo.js
@@ -0,0 +1,10 @@
+/* 
+ Sours:          index.html
+Description:    Index del programa 1
+Date:           
+ */
+/* este es otra manera de arrojar un mensaje creando un javascrit file*/
+alert("Hola Mundo desde ARCHIVO JS");
+
+
+
---
+++ TopicosAvanzados19A/public_html/Oscar_Uh/program03.js
@@ -0,0 +1,7 @@
+/* 
+ Sours:          index.html
+Description:    Index del programa 3
+Date:           14 de Febrero de 2019
+ */
+
+
