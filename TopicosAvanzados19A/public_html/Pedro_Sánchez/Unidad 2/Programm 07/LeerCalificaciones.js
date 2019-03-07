ae7024e0f6a7294ce5d65fd529716d302d100a41
Pedro Adrián Sánchez Cárdenas <pedrosanchezcardenas@MacBook-Pro-de-Pedro-Sanchez.local>
2019-03-07 09:47:28 -0600

    Actualizacion Programm07

--- TopicosAvanzados19A/public_html/Pedro_Sánchez/Unidad 2/Programm 07/LeerCalificaciones.js
+++ TopicosAvanzados19A/public_html/Pedro_Sánchez/Unidad 2/Programm 07/LeerCalificaciones.js
@@ -2,28 +2,41 @@
 *
 */
 
-var grupo;
+var grupo=0;
 var cal = [];
 var suma = 0;
 var i;
 
 function getCantidadAlumnos(){
-	while(true){
-		grupo = Number(prompt("Escribe la cantidad de alumnos:"));
-		if(grupo%1 === 0 && grupo >=0){
-			break;
-		}else{
-			alert("Escribe un entero positivo");
-		}
-	}
-
+  while(true){
+    grupo = Number(prompt("Escribela Cantidad de Alumnos"));
+    if(grupo %1===0 && grupo >=0){
+      break;
+    }else{
+      alert("Escribe un entero positivo")
+    }
+  }
 }
 
-function LeerCalificaciones(){
-	for(i _= 0; i < grupo.size(); i++){
-		while(true){
-			var calificaciones = prompt("Escribe la calificación del alumno " + i + ": ");
-		}
+//función para leer las calificaciónes
+function leerCalificaciones() {
+    //Ciclo for en el que se validan las calificaciones.
+    for (i = 0; i < grupo; i++) {
+        while (true) {
+		    calificacion = Number(prompt("Escribela la calificacion"));
+		    if(calificacion %1===0 && calificacion >=0 && calificacion<=100){
+		    	cal[i]=Number(calificacion);
+		      break;
+		    }else{
+		      alert("Escribe un entero positivo")
+		    }
+        
+    	}
+	    suma = suma + cal[i];
 	}
+}
 
+function getPromedio(){
+	var promedio = Number(suma)/Number(grupo);
+	alert("El promedio de calificaciones es: "+ promedio);
 }
