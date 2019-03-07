6410525264ff9720997588dac7410e750145a24b
Jonathan Euan Sanchez <euan_jonathan@live.com.mx>
2019-03-07 08:15:55 -0600

    actualizacion

--- TopicosAvanzados19A/public_html/JonathanEuan/Program02.js
+++ TopicosAvanzados19A/public_html/JonathanEuan/Program02.js
@@ -1,4 +1,10 @@
-
+/*
+Descripcion: PROGRAMA PARA LEER CALIFICACIONES DE UN GRUPO
+1. LEER N (TAMAÑO DE GRUPO). LEER CALIFICACIONES
+2. CONSERVAR LAS CALIFICACIONES(ARREGLO). CALCULAR EL PROMEDIO
+    (SUMAR CALIFICACIONES Y DIVIDIR ENTRE N)
+3. IMPRIMIR LAS CALIFICACIONES. IMPRIMIR EL PROMEDIO.
+*/
 
 function leerCalificaciones() {
     var grupo = document.getElementById("inputGrupo").value;
@@ -8,17 +14,20 @@ function leerCalificaciones() {
     for (i = 0; i < grupo; i++) {
         while (true) {
             var calificaciones = prompt("escribe la calificación del alumno");
-          /*linea de codigo que muestra una ventana donde inserter lo solicitado
+          /*linea de codigo que muestra una ventana donde inserter lo solicitado,
+          en este parte el usuario ingresa las calificaciones de todos los alumnos.
           */
             cal[i] = Number(calificaciones);
             if (cal[i] % 1 === 0 && cal[i] >= 0 && cal[i] <= 100) {
-/* si la calificaion es entera positiva, mayor igual cero y/o menor igual 100 */
+/* si la calificaion es entera positiva, mayor igual cero y/o menor igual 100
+en caso de no ser una calificacion valida, se ejecuta el else.*/
                 break;
             } else {
                 alert("No es una calificacion valida");
             }
         }
         suma = suma + cal[i];
+  /* En esta parte se suman las calificaciones */
     }
     document.write(suma / grupo);
 }
--- TopicosAvanzados19A/public_html/JonathanEuan/codigoJS.js
+++ TopicosAvanzados19A/public_html/JonathanEuan/codigoJS.js
@@ -1,4 +1,4 @@
 /*Programa 01
 creacion de un html como introduccion a la primera unidad
 de Topicos avanzados de programacion */
-alert("HolaMundo desde archivo")
+alert("HolaMundo desde archivo");
