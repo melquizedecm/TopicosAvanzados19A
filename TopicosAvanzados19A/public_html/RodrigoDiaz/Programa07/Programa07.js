74e1c68e2a03091652b14aac039bcb369fd72d7a
RodrigoDiaz99 <diaz-rodrigo@hotmail.com>
2019-03-13 16:01:16 -0600

    Act
    Act

---
+++ TopicosAvanzados19A/public_html/RodrigoDiaz/Programa07/Programa07.js
@@ -0,0 +1,44 @@
+/*source: Programa02.js
+Description: Programa para leer calificaciones de un grupo 
+1.leer n(tamaño de grupo).Leer calificaciones
+2. Conservar las calificaciones(arreglo).Calcular el promedio
+(sumar calificaciones y dividir entre n)
+3.Imprimir las calificaciones. Imprimir el promedio
+*/
+
+var grupo=0;
+var cal=[];
+var suma=0;
+var i;
+ 
+ function getCantidadAlumnos(){
+ 	while(true){
+ 		grupo=Number(prompt("Escribe la Cantidad de Alumnos"));
+ 		if(grupo %1===0 && grupo >=0){
+
+
+
+
+
+
+ 			
+ 		}
+
+
+
+
+
+
+
+
+
+ 	}
+
+
+
+
+
+
+
+
+ }
