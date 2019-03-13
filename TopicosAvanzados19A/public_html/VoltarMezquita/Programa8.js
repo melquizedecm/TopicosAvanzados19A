469ba2d43cc4e27e6d6acd9c759999fa218c8a9d
Pedro Adrián Sánchez Cárdenas <pedrosanchezcardenas@MacBook-Pro-de-Pedro-Sanchez.local>
2019-03-13 15:32:40 -0600

    Program 09 completo carpeta Frank Montalvo

--- TopicosAvanzados19A/public_html/VoltarMezquita/Programa8.js
+++ TopicosAvanzados19A/public_html/VoltarMezquita/Programa8.js
@@ -6,35 +6,40 @@ var T3;
 var X;
 var p;
 var Resultado;
+var  F=false;
+
 function Rango(){
-N=prompt('Ingresa el rango minimo');
-N2=prompt('Ingresa el rango maximo');
-N3=prompt('Numero a comparar en el rango');
-if(N>=N3 && N2<=N3){
+  N=prompt('Ingresa el rango minimo');
+  N2=prompt('Ingresa el rango maximo');
+  N3=prompt('Numero a comparar en el rango');
+  if(N>=N3 && N2<=N3){
   alert("El numero esta en el rango");
-}else{
+  boolean(N);
+    }else{
   alert("El numero no esta en el rango");
-}
+  boolean(F);
+  }
 }
 
 function MAYUSCULAS() {
   T=prompt('Ingrese texto');
   var T2=T.toUpperCase();
-    alert(T2);
+boolean(T==T2);
 }
 
 function minusculas(){
   T3=prompt('Ingrese texto');
   var T4=T3.toLowerCase();
-  alert(T4);
+  boolean(T3==T4);
 }
 
 function Entero(){
   X=prompt('Numero a convertir en entero');
   var Y=Math.round(X)
-  alert(Y);
+boolean(X==Y);
 }
 
 function Flotante(){
   P=parseFloat( Resultado )
+  boolean(P== Resultado);
 }
