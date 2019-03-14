9fef93175a2940d7e8c638ca34c600f10826f621
KarenJanett <pe.ke2010@hotmail.com>
2019-03-14 07:30:53 -0600

    .commit para Angular

--- TopicosAvanzados19A/public_html/Karen_Lopéz/AngularJS/public_html/angular/js/directivas.js
+++
@@ -1,9 +0,0 @@
-app.directive('tooltip', function() {
-  return {
-    restrict: 'EA',
-    link: function(scope, element, attrs) {
-      attrs.tooltipTrigger = attrs.tooltipTrigger;
-      attrs.tooltipPlacement = attrs.tooltipPlacement || 'top';
-    }
-  }
-});
