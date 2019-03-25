3752d0259280ec3bf39b3044af68f98457de51ed
Jonathan Euan Sanchez <euan_jonathan@live.com.mx>
2019-03-25 17:12:45 -0600

    Actualizaciones

---
+++ TopicosAvanzados19A/public_html/JonathanEuan/U2/Programm14.js
@@ -0,0 +1,20 @@
+
+      function caballo(etiquetaNombre,metros, fin){
+        var contador=0;
+        var i=0;
+        while(i<=fin){
+            nombre=document.getElementById(etiquetaNombre).value;
+        document.getElementById(metros).value= nombre + " " + i + "mts";
+        i++;
+        }
+        nombre=document.getElementById(etiquetaNombre).value;
+        alert("Gano el caballo: "+nombre);
+
+
+      }
+
+    function ejecutar(){
+      Concurrent.Thread.create(caballo,"inputCaballo1","metrosRecorridos1",100000);
+      Concurrent.Thread.create(caballo,"inputCaballo2","metrosRecorridos2",100000);
+      Concurrent.Thread.create(caballo,"inputCaballo3","metrosRecorridos3",100000);
+    }
