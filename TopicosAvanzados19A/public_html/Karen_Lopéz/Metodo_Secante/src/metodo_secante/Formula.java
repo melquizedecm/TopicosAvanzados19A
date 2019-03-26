d5d5bb07d9b4463f765e42df515a4f4c4e55c74c
KarenJanett <pe.ke2010@hotmail.com>
2019-03-25 20:02:56 -0600

    ,

--- TopicosAvanzados19A/public_html/Karen_Lopéz/Metodo_Secante/src/metodo_secante/Formula.java
+++ TopicosAvanzados19A/public_html/Karen_Lopéz/Metodo_Secante/src/metodo_secante/Formula.java
@@ -1,39 +1,39 @@
 package metodo_secante;
 import java.util.Scanner;
-import static javafx.scene.input.KeyCode.T;
 
 public class Formula {
-    
- public void diametroE(){
-          double e = 0;
-          double Øe = 0;
-          e=(-J(Math.pow(Øe,4))+(16(T/Math.PI))Øe+((Math.pow(Øi,4)J)));
-       }
- 
-     public static void main(String[] args) {
-         //pedir 5 valores
-         // F(Øe)=-JØe^4+16(T/pi)Øe+Øi^4J ---> buscar el diametro exterior
-         //xi+1= xi-F(xi)(xi-1-xi)/F(xi-1)-(F(xi))
-         int J=0;
-         int T=0;
-         double Øi=0;
-         String Xi=""; 
-         String XI="";
-         Scanner z=new Scanner(System.in);
-         
-         System.out.println("ingrese el J: ");
-         J=z.nextInt();
-         System.out.println("ingrese el T: ");
-         T=z.nextInt();
-         System.out.println("ingrese el Øi: ");
-         Øi=z.nextDouble();
-         System.out.println("ingrese el Xi: ");
-         Xi=z.next();
-         System.out.println("ingrese el Xi-1: ");
-         XI=z.next();
-     }
-
-    private int J(double pow) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	
+	public void diametroE(double t, int T, double Øi){
+        double e=0;
+        e=(int)(-t+Math.round(Math.pow(e,4))+(16*(T/Math.PI)));
     }
+        
+  public static void main(String[] args) {
+      //pedir 5 valores
+      // F(Øe)=-JØe^4+16(T/pi)Øe+Øi^4J ---> buscar el diametro exterior
+      //xi+1= xi-F(xi)(xi-1-xi)/F(xi-1)-(F(xi))
+	  
+      double t=0;  //esfuerzo
+      int T=0;     // par
+      double Øi=0;  //diametro interior
+      String X1="";  
+      String X2="";
+      Scanner z=new Scanner(System.in);
+      
+      System.out.println("ingrese el t: ");
+      t=z.nextDouble();
+      System.out.println("ingrese el T: ");
+      T=z.nextInt();
+      System.out.println("ingrese el Øi: ");
+      Øi=z.nextDouble();
+      System.out.println("ingrese el X1: ");
+      X1=z.next();
+      System.out.println("ingrese el X2: ");
+      X2=z.next();
+    
+    
+     //imprimir los datos de la funcion
+     Formula s=new Formula();
+     System.out.println("el resultado es "+s);
+  }
 }
