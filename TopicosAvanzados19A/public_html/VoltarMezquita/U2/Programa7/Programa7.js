ebb6046924225af6ff1c86d1e0e1f00483ab5bcc
jack mezquita <jack_vroj_1298@hotmail.com>
2019-04-30 08:38:51 -0500

    programa7 comentario

--- TopicosAvanzados19A/public_html/VoltarMezquita/U2/Programa7/Programa7.js
+++ TopicosAvanzados19A/public_html/VoltarMezquita/U2/Programa7/Programa7.js
@@ -13,27 +13,25 @@ var suma=0;
 var promedio;
 var i;
 var bandera=true;
-leerCalificaciones();
 
+leerCalificaciones();
 function GetCantidadAlumnos(){
   while(true){      <!-- funcion para ingresar cantidada de alunos-->
     Grupo = Number(prompt("Escribir la antidad de alumnos"));
-    if(grupo %1===0 && Grupo>=0){     <!-- si es diferente a 0 y mayor que 0 sino lo vuelve a escribir-->
+    if(grupo %1===0 && Grupo>=0){     //si es diferente a 0 y mayor que 0 sino lo vuelve a escribir-->
       break;
     }else{
       alert("Escribe un entero positivo");
     }
   }
 }
-
-function leerCalificaciones(){      <!-- funcion para leer calificaion-->
+function leerCalificaciones(){      //funcion para leer calificaion
   do{
     n=prompt("Escribe el numero de alumnos"); //imprimir
   }while(Number.isInteger(n)==false&& !(n>0)); //si n es entero
 document.write(n);
-
 }
-function OptenerPromedio(){   <!-- funcion para sacar promedio-->
+function OptenerPromedio(){   //uncion para sacar promedio
   promedio=suma/grupo;
 alert(Promedio);
 suma=0;
